d194c0a0596ebba560b3ef79db573156acb0ce5f
Feng Gao <gaof.student@gmail.com>
2019-01-30 23:58:26 +0800

    add depthnet to model.py

--- pytorch_version/model.py
+++ pytorch_version/model.py
@@ -131,7 +131,7 @@ class SE3_Generator_KITTI(torch.autograd.Function):
 generate_se3 = SE3_Generator_KITTI.apply
 
 class OdometryNet(nn.Module):
-    
+
     def __init__(self):
         super(OdometryNet, self).__init__()
         self.img_width = 608
@@ -196,10 +196,138 @@ class DepthNet(nn.Module):
 
         self.eltwise_stage0_block0 = nn.ReLU(inplace=True)
 
+        self.conv_stage0_block1_branch2a = conv_bn_relu(128, 32, kernel_size=1, padding=0, stride=1)
+        self.conv_stage0_block1_branch2b = conv_bn_relu(32, 32, kernel_size=3, padding=1, stride=1)
+        self.conv_stage0_block1_branch2c = conv_bn(32, 128, kernel_size=1, padding=0, stride=1)
+
+        self.eltwise_stage0_block1 = nn.ReLU(inplace=True)
+
+        self.conv_stage0_block2_branch2a = conv_bn_relu(128, 32, kernel_size=1, padding=0, stride=1)
+        self.conv_stage0_block2_branch2b = conv_bn_relu(32, 32, kernel_size=3, padding=1, stride=1)
+        self.conv_stage0_block2_branch2c = conv_bn(32, 128, kernel_size=1, padding=0, stride=1)
+
+        self.eltwise_stage0_block2 = nn.ReLU(inplace=True)
+
+        # bottom: eltwise_stage0_block2
+        self.conv_stage1_block0_proj_shortcut = conv_bn(128, 256, kernel_size=1, padding=0, stride=2)
+
+        # bottom: eltwise_stage0_block2
+        self.conv_stage1_block0_branch2a = conv_bn_relu(128, 64, kernel_size=1, padding=0, stride=2)
+        self.conv_stage1_block0_branch2b = conv_bn_relu(64, 64, kernel_size=3, padding=1, stride=1)
+        self.conv_stage1_block0_branch2c = conv_bn(64, 256, kernel_size=1, padding=0, stride=1)
+
+        self.eltwise_stage1_block0 = nn.ReLU(inplace=True)
+
+        self.conv_stage1_block1_branch2a = conv_bn_relu(256, 64, kernel_size=1, padding=0, stride=1)
+        self.conv_stage1_block1_branch2b = conv_bn_relu(64, 64, kernel_size=3, padding=1, stride=1)
+        self.conv_stage1_block1_branch2c = conv_bn(64, 256, kernel_size=1, padding=0, stride=1)
+
+        self.eltwise_stage1_block1 = nn.ReLU(inplace=True)
+
+        self.conv_stage1_block2_branch2a = conv_bn_relu(256, 64, kernel_size=1, padding=0, stride=1)
+        self.conv_stage1_block2_branch2b = conv_bn_relu(64, 64, kernel_size=3, padding=1, stride=1)
+        self.conv_stage1_block2_branch2c = conv_bn(64, 256, kernel_size=1, padding=0, stride=1)
+
+        self.eltwise_stage1_block2 = nn.ReLU(inplace=True)
+
+        self.conv_stage1_block3_branch2a = conv_bn_relu(256, 64, kernel_size=1, padding=0, stride=1)
+        self.conv_stage1_block3_branch2b = conv_bn_relu(64, 64, kernel_size=3, padding=1, stride=1)
+        self.conv_stage1_block3_branch2c = conv_bn(64, 256, kernel_size=1, padding=0, stride=1)
+
+        self.eltwise_stage1_block3 = nn.ReLU(inplace=True)
+
+        # bottom: eltwise_stage1_block3
+        self.conv_stage2_block0_proj_shortcut = conv_bn(256, 512, kernel_size=1, padding=0, stride=2)
+
+        # bottom: eltwise_stage1_block3
+        self.conv_stage2_block0_branch2a = conv_bn_relu(256, 128, kernel_size=1, padding=0, stride=2)
+        self.conv_stage2_block0_branch2b = conv_bn_relu(128, 128, kernel_size=3, padding=1, stride=1)
+        self.conv_stage2_block0_branch2c = conv_bn(128, 512, kernel_size=1, padding=0, stride=1)
+
+        self.eltwise_stage2_block0 = nn.ReLU(inplace=True)
+
+        self.conv_stage2_block1_branch2a = conv_bn_relu(512, 128, kernel_size=1, padding=0, stride=1)
+        self.conv_stage2_block1_branch2b = conv_bn_relu(128, 128, kernel_size=3, padding=1, stride=1)
+        self.conv_stage2_block1_branch2c = conv_bn(128, 512, kernel_size=1, padding=0, stride=1)
+
+        self.eltwise_stage2_block1 = nn.ReLU(inplace=True)
+
+        self.conv_stage2_block2_branch2a = conv_bn_relu(512, 128, kernel_size=1, padding=0, stride=1)
+        self.conv_stage2_block2_branch2b = conv_bn_relu(128, 128, kernel_size=3, padding=1, stride=1)
+        self.conv_stage2_block2_branch2c = conv_bn(128, 512, kernel_size=1, padding=0, stride=1)
+
+        self.eltwise_stage2_block2 = nn.ReLU(inplace=True)
+
+        self.conv_stage2_block3_branch2a = conv_bn_relu(512, 128, kernel_size=1, padding=0, stride=1)
+        self.conv_stage2_block3_branch2b = conv_bn_relu(128, 128, kernel_size=3, padding=1, stride=1)
+        self.conv_stage2_block3_branch2c = conv_bn(128, 512, kernel_size=1, padding=0, stride=1)
+
+        self.eltwise_stage2_block3 = nn.ReLU(inplace=True)
+
+        self.conv_stage2_block4_branch2a = conv_bn_relu(512, 128, kernel_size=1, padding=0, stride=1)
+        self.conv_stage2_block4_branch2b = conv_bn_relu(128, 128, kernel_size=3, padding=1, stride=1)
+        self.conv_stage2_block4_branch2c = conv_bn(128, 512, kernel_size=1, padding=0, stride=1)
+
+        self.eltwise_stage2_block4 = nn.ReLU(inplace=True)
+
+        self.conv_stage2_block5_branch2a = conv_bn_relu(512, 128, kernel_size=1, padding=0, stride=1)
+        self.conv_stage2_block5_branch2b = conv_bn_relu(128, 128, kernel_size=3, padding=1, stride=1)
+        self.conv_stage2_block5_branch2c = conv_bn(128, 512, kernel_size=1, padding=0, stride=1)
+
+        self.eltwise_stage2_block5 = nn.ReLU(inplace=True)
+
+        # bottom: eltwise_stage2_block5
+        self.conv_stage3_block0_proj_shortcut = conv_bn(512, 1024, kernel_size=1, padding=0, stride=2)
+
+        # bottom: eltwise_stage1_block5
+        self.conv_stage3_block0_branch2a = conv_bn_relu(512, 256, kernel_size=1, padding=0, stride=2)
+        self.conv_stage3_block0_branch2b = conv_bn_relu(256, 256, kernel_size=3, padding=1, stride=1)
+        self.conv_stage3_block0_branch2c = conv_bn(256, 1024, kernel_size=1, padding=0, stride=1)
+
+        self.eltwise_stage3_block0 = nn.ReLU(inplace=True)
+
+        self.conv_stage3_block1_branch2a = conv_bn_relu(1024, 256, kernel_size=1, padding=0, stride=1)
+        self.conv_stage3_block1_branch2b = conv_bn_relu(256, 256, kernel_size=3, padding=1, stride=1)
+        self.conv_stage3_block1_branch2c = conv_bn(256, 1024, kernel_size=1, padding=0, stride=1)
+
+        self.eltwise_stage3_block1 = nn.ReLU(inplace=True)
+
+        self.conv_stage3_block2_branch2a = conv_bn_relu(1024, 256, kernel_size=1, padding=0, stride=1)
+        self.conv_stage3_block2_branch2b = conv_bn_relu(256, 256, kernel_size=3, padding=1, stride=1)
+        self.conv_stage3_block2_branch2c = conv_bn(256, 1024, kernel_size=1, padding=0, stride=1)
+
+        self.eltwise_stage3_block2 = nn.ReLU(inplace=True)
+
+        # score_fr
+        self.score_fr = nn.Conv2d(1024, 1, kernel_size=1, padding=0, stride=1)
+
+        # upscore_pool5
+        self.upscore_pool5 = nn.ConvTranspose2d(1, 1, kernel_size=4, padding=1, stride=2, group=1)
+        self.score_pool4 = nn.Conv2d(512, 1, kernel_size=1, padding=0, stride=1)
+
+        # upscore_pool4
+        self.upscore_pool4 = nn.ConvTranspose2d(1, 1, kernel_size=4, padding=1, stride=2, group=1)
+        self.score_pool3 = nn.Conv2d(256, 1, kernel_size=1, padding=0, stride=1)
+
+        # upscore_pool3
+        self.upscore_pool3 = nn.ConvTranspose2d(1, 1, kernel_size=4, padding=1, stride=2, group=1)
+        self.score_pool2 = nn.Conv2d(128, 1, kernel_size=1, padding=0, stride=1)
+
+        # upscore_pool2
+        self.upscore_pool2 = nn.ConvTranspose2d(1, 1, kernel_size=4, padding=1, stride=2, group=1)
+        self.score_pool1 = nn.Conv2d(64, 1, kernel_size=1, padding=0, stride=1)
+
+        # upscore_all
+        self.upscore_all = nn.ConvTranspose2d(1, 1, kernel_size=4, padding=1, stride=2, group=1)
+        
+        # inv_depth
+        self.inv_depth = nn.ReLU(inplace=True)
+
     def forward(self, x):
         conv_1 = self.conv_1(x)
         pool1 = self.pool1(conv_1)
 
+        # stage 0
         conv_stage0_block0_proj_shortcut = self.conv_stage0_block0_proj_shortcut(pool1)
 
         conv_stage0_block0_branch2a = self.conv_stage0_block0_branch2a(pool1)
@@ -208,4 +336,132 @@ class DepthNet(nn.Module):
 
         eltwise_stage0_block0 = self.eltwise_stage0_block0(conv_stage0_block0_proj_shortcut + conv_stage0_block0_branch2c)
 
-        pass
+        conv_stage0_block1_branch2a = self.conv_stage0_block1_branch2a(eltwise_stage0_block0)
+        conv_stage0_block1_branch2b = self.conv_stage0_block1_branch2b(conv_stage0_block1_branch2a)
+        conv_stage0_block1_branch2c = self.conv_stage0_block1_branch2b(conv_stage0_block1_branch2b)
+
+        eltwise_stage0_block1 = self.eltwise_stage0_block1(eltwise_stage0_block0 + conv_stage0_block1_branch2c)
+
+        conv_stage0_block2_branch2a = self.conv_stage0_block2_branch2a(eltwise_stage0_block1)
+        conv_stage0_block2_branch2b = self.conv_stage0_block2_branch2b(conv_stage0_block2_branch2a)
+        conv_stage0_block2_branch2c = self.conv_stage0_block2_branch2b(conv_stage0_block2_branch2b)
+
+        eltwise_stage0_block2 = self.eltwise_stage0_block2(eltwise_stage0_block1 + conv_stage0_block2_branch2c)
+
+        # stage 1
+        conv_stage1_block0_proj_shortcut = self.conv_stage1_block0_proj_shortcut(eltwise_stage0_block2)
+
+        conv_stage1_block0_branch2a = self.conv_stage1_block0_branch2a(eltwise_stage0_block2)
+        conv_stage1_block0_branch2b = self.conv_stage1_block0_branch2b(conv_stage1_block0_branch2a)
+        conv_stage1_block0_branch2c = self.conv_stage1_block0_branch2b(conv_stage1_block0_branch2b)
+
+        eltwise_stage1_block0 = self.eltwise_stage1_block0(conv_stage1_block0_proj_shortcut + conv_stage1_block0_branch2c)
+
+        conv_stage1_block1_branch2a = self.conv_stage1_block1_branch2a(eltwise_stage1_block0)
+        conv_stage1_block1_branch2b = self.conv_stage1_block1_branch2b(conv_stage1_block1_branch2a)
+        conv_stage1_block1_branch2c = self.conv_stage1_block1_branch2b(conv_stage1_block1_branch2b)
+
+        eltwise_stage1_block1 = self.eltwise_stage1_block1(eltwise_stage1_block0 + conv_stage1_block1_branch2c)
+
+        conv_stage1_block2_branch2a = self.conv_stage1_block2_branch2a(eltwise_stage1_block1)
+        conv_stage1_block2_branch2b = self.conv_stage1_block2_branch2b(conv_stage1_block2_branch2a)
+        conv_stage1_block2_branch2c = self.conv_stage1_block2_branch2b(conv_stage1_block2_branch2b)
+
+        eltwise_stage1_block2 = self.eltwise_stage1_block2(eltwise_stage1_block1 + conv_stage1_block2_branch2c)
+
+        conv_stage1_block3_branch2a = self.conv_stage1_block3_branch2a(eltwise_stage1_block2)
+        conv_stage1_block3_branch2b = self.conv_stage1_block3_branch2b(conv_stage1_block3_branch2a)
+        conv_stage1_block3_branch2c = self.conv_stage1_block3_branch2b(conv_stage1_block3_branch2b)
+
+        eltwise_stage1_block3 = self.eltwise_stage1_block3(eltwise_stage1_block2 + conv_stage1_block3_branch2c)
+
+        # stage 2
+        conv_stage2_block0_proj_shortcut = self.conv_stage2_block0_proj_shortcut(eltwise_stage1_block3)
+
+        conv_stage2_block0_branch2a = self.conv_stage2_block0_branch2a(eltwise_stage1_block3)
+        conv_stage2_block0_branch2b = self.conv_stage2_block0_branch2b(conv_stage2_block0_branch2a)
+        conv_stage2_block0_branch2c = self.conv_stage2_block0_branch2b(conv_stage2_block0_branch2b)
+
+        eltwise_stage2_block0 = self.eltwise_stage2_block0(conv_stage2_block0_proj_shortcut + conv_stage2_block0_branch2c)
+
+        conv_stage2_block1_branch2a = self.conv_stage2_block1_branch2a(eltwise_stage2_block0)
+        conv_stage2_block1_branch2b = self.conv_stage2_block1_branch2b(conv_stage2_block1_branch2a)
+        conv_stage2_block1_branch2c = self.conv_stage2_block1_branch2b(conv_stage2_block1_branch2b)
+
+        eltwise_stage2_block1 = self.eltwise_stage2_block1(eltwise_stage2_block0 + conv_stage2_block1_branch2c)
+
+        conv_stage2_block2_branch2a = self.conv_stage2_block2_branch2a(eltwise_stage2_block1)
+        conv_stage2_block2_branch2b = self.conv_stage2_block2_branch2b(conv_stage2_block2_branch2a)
+        conv_stage2_block2_branch2c = self.conv_stage2_block2_branch2b(conv_stage2_block2_branch2b)
+
+        eltwise_stage2_block2 = self.eltwise_stage2_block2(eltwise_stage2_block1 + conv_stage2_block2_branch2c)
+
+        conv_stage2_block3_branch2a = self.conv_stage2_block3_branch2a(eltwise_stage2_block2)
+        conv_stage2_block3_branch2b = self.conv_stage2_block3_branch2b(conv_stage2_block3_branch2a)
+        conv_stage2_block3_branch2c = self.conv_stage2_block3_branch2b(conv_stage2_block3_branch2b)
+
+        eltwise_stage2_block3 = self.eltwise_stage2_block3(eltwise_stage2_block2 + conv_stage2_block3_branch2c)
+
+        conv_stage2_block4_branch2a = self.conv_stage2_block4_branch2a(eltwise_stage2_block3)
+        conv_stage2_block4_branch2b = self.conv_stage2_block4_branch2b(conv_stage2_block4_branch2a)
+        conv_stage2_block4_branch2c = self.conv_stage2_block4_branch2b(conv_stage2_block4_branch2b)
+
+        eltwise_stage2_block4 = self.eltwise_stage2_block4(eltwise_stage2_block3 + conv_stage2_block4_branch2c)
+
+        conv_stage2_block5_branch2a = self.conv_stage2_block5_branch2a(eltwise_stage2_block4)
+        conv_stage2_block5_branch2b = self.conv_stage2_block5_branch2b(conv_stage2_block5_branch2a)
+        conv_stage2_block5_branch2c = self.conv_stage2_block5_branch2b(conv_stage2_block5_branch2b)
+
+        eltwise_stage2_block5 = self.eltwise_stage2_block5(eltwise_stage2_block4 + conv_stage2_block5_branch2c)
+
+        # stage 3
+        conv_stage3_block0_proj_shortcut = self.conv_stage3_block0_proj_shortcut(eltwise_stage2_block5)
+
+        conv_stage3_block0_branch2a = self.conv_stage3_block0_branch2a(eltwise_stage2_block5)
+        conv_stage3_block0_branch2b = self.conv_stage3_block0_branch2b(conv_stage3_block0_branch2a)
+        conv_stage3_block0_branch2c = self.conv_stage3_block0_branch2b(conv_stage3_block0_branch2b)
+
+        eltwise_stage3_block0 = self.eltwise_stage3_block0(conv_stage3_block0_proj_shortcut + conv_stage3_block0_branch2c)
+
+        conv_stage3_block1_branch2a = self.conv_stage3_block1_branch2a(eltwise_stage3_block0)
+        conv_stage3_block1_branch2b = self.conv_stage3_block1_branch2b(conv_stage3_block1_branch2a)
+        conv_stage3_block1_branch2c = self.conv_stage3_block1_branch2b(conv_stage3_block1_branch2b)
+
+        eltwise_stage3_block1 = self.eltwise_stage3_block1(eltwise_stage3_block0 + conv_stage3_block1_branch2c)
+
+        conv_stage3_block2_branch2a = self.conv_stage3_block2_branch2a(eltwise_stage3_block1)
+        conv_stage3_block2_branch2b = self.conv_stage3_block2_branch2b(conv_stage3_block2_branch2a)
+        conv_stage3_block2_branch2c = self.conv_stage3_block2_branch2b(conv_stage3_block2_branch2b)
+
+        eltwise_stage3_block2 = self.eltwise_stage3_block2(eltwise_stage3_block1 + conv_stage3_block2_branch2c)
+
+        # score_fr
+        score_fr = self.score_fr(eltwise_stage3_block2)
+
+        # upscore_pool5
+        upscore_pool5 = 2 * self.upscore_pool5(score_fr)
+        scale_pool4 = self.score_pool4(0.01 * eltwise_stage2_block5)
+        fuse_pool4 = upscore_pool5 + scale_pool4
+
+        # upscore_pool4
+        upscore_pool4 = 2 * self.upscore_pool4(fuse_pool4)
+        scale_pool3 = self.score_pool3(0.0001 * eltwise_stage1_block3)
+        fuse_pool3 = upscore_pool4 + scale_pool3
+
+        # upscore_pool3
+        upscore_pool3 = 2 * self.upscore_pool3(fuse_pool3)
+        scale_pool2 = self.score_pool2(0.00001 * eltwise_stage0_block2)
+        fuse_pool2 = upscore_pool3 + scale_pool2
+
+        # upscore_pool2
+        upscore_pool2 = 2 * self.upscore_pool2(fuse_pool2)
+        scale_pool1 = self.score_pool1(0.000001 * conv_1)
+        fuse_pool1 = upscore_pool2 + scale_pool1
+
+        # upscore_all
+        upscore_all = self.upscore_all(fuse_pool1)
+
+        # inv_depth
+        inv_depth = self.inv_depth(0.01 * upscore_all)
+
+        return inv_depth
